1639871a8ddfc5e21d67da9444914f00d58cc121
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-08-15 23:52:17 +0530

    something changed

--- app.js
+++ app.js
@@ -13,60 +13,43 @@ form.addEventListener("submit", (e) => {
     // console.log(purchasePrice, stockQuantity, currentPrice)
 
     if (purchasePrice > 0 && stockQuantity > 0 && currentPrice > 0) {
+
         if (purchasePrice > currentPrice) {
             //total loss in cash
+            
             const totalLoss = ((purchasePrice - currentPrice) * stockQuantity).toFixed(2);
             const lossPerce = (((purchasePrice - currentPrice) * 100) / purchasePrice).toFixed(2);
 
             if (lossPerce > 50) {
-                document.querySelector(".background").style.backgroundImage =
-                    "url('/gif2.webp')";
+                document.querySelector(".background").style.backgroundImage = "url('/gif2.webp')";
                 document.querySelector(".background").style.backgroundSize = "cover";
-                document.querySelector(".background").style.backgroundPosition =
-                    "center";
+                document.querySelector(".background").style.backgroundPosition = "center";
             }
             var losses = "";
             losses = `
-            
                     <div class="loss-info">
-                    
                         <p> You lost ${lossPerce}%, and Your total loss is ₹${totalLoss}</p>
-
                     </div>
-
             `;
             document.querySelector(".output").innerHTML = losses;
-
         } else {
+
             //total profit in cash
-            const totalProfit = (
-                (currentPrice - purchasePrice) *
-                stockQuantity
-            ).toFixed(2);
+            const totalProfit = ((currentPrice - purchasePrice) *stockQuantity).toFixed(2);
             //profit in percentage
-            const profitPer = (
-                ((currentPrice - purchasePrice) * 100) /
-                purchasePrice
-            ).toFixed(2);
+            const profitPer = (((currentPrice - purchasePrice) * 100) /purchasePrice).toFixed(2);
             // profitPercentage(profitPer)
 
             if (profitPer > 50) {
-                document.querySelector(".background").style.backgroundImage =
-                    "url('/giphy.webp')";
+                document.querySelector(".background").style.backgroundImage ="url('/giphy.webp')";
                 document.querySelector(".background").style.backgroundSize = "cover";
-                document.querySelector(".background").style.backgroundPosition =
-                    "center";
+                document.querySelector(".background").style.backgroundPosition ="center";
             }
-
             var prof = "";
             prof = `
-            
                     <div class="loss-info">
-                    
                         <p> You gain ${profitPer}%, and Your total profit is ₹${totalProfit}</p>
-
                     </div>
-
             `;
             document.querySelector(".output").innerHTML = prof;
         }
---
+++ stock.js
@@ -0,0 +1,16 @@
+// fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=tesla&region=US", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "7fd6e8d21bmshe7ed01027a502d5p1545c6jsn42215d7d0de2",
+// 		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+// 	}
+// })
+// .then(response => {
+// 	return response.json()
+// })
+// .then((data) =>{
+//     console.log(data)
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
